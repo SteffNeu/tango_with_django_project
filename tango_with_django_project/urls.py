8112c7420ba17b66e9d226d46130c5de824beb70
SteffNeu <2351944n@student.gla.ac.uk>
2018-01-23 18:05:14 +0000

    Chapter 6 complete

--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -21,8 +21,6 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-	url(r'^$', views.index, name='index'),
-	url(r'^$', views.about, name='about'),
 	url(r'^rango/', include('rango.urls')),
 	# above maps any URLs starting
 	# with rango/ to be handled bytearray
